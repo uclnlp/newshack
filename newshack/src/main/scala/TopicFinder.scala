e0637e99888d978d25f39dfc2e7739c5abac58c6
riedelcastro <sebastian.riedel@gmail.com>
2013-10-17 13:24:07 +0100

    refactoring and merging of results

--- newshack/src/main/scala/TopicFinder.scala
+++ newshack/src/main/scala/TopicFinder.scala
@@ -8,28 +8,40 @@ import org.riedelcastro.frontlets.Frontlet
 object TopicFinder {
 
   class JuicerResult extends Frontlet {
-    val coOccurences = FrontletListSlot("co-occurrences", () => new Entity)
+    val coOccurrences = FrontletListSlot("co-occurrences", () => new Entity)
   }
 
   class Entity extends Frontlet {
     val label = StringSlot("label")
+    val occurrence = StringSlot("occurrence")
   }
+
 
   def main(args: Array[String]) {
     val dbpediaIds = args
+    val result = searchByCooccurrence(dbpediaIds)
+    println(result.mkString("\n"))
+  }
+
+  def cooccurrenceQuery(id:String,typ:String = "http://dbpedia.org/ontology/Person", limit:Int = 5) = {
     val result = Http("http://triplestore.bbcnewslabs.co.uk//api/concepts/co-occurrences").
       option(_.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2")).
       option(HttpOptions.connTimeout(10000)).
       option(HttpOptions.readTimeout(10000)).params(
-      "concept" -> "http://dbpedia.org/resource/David_Cameron",
-      "type" -> "http://dbpedia.org/ontology/Person",
-      "limit" -> "5"
+      "concept" -> id,
+      "type" -> typ,
+      "limit" -> limit.toString
     )
-    val juicerResult = new JuicerResult().setJSON(result.asString)
-    val label = juicerResult.coOccurences().head.label()
-    println(label)
+    new JuicerResult().setJSON(result.asString)
+
+  }
 
 
+  def searchByCooccurrence(ids:Seq[String]):Seq[Entity] = {
+    val perIdResults = ids.map(id => cooccurrenceQuery(id).coOccurrences())
+    val merged = perIdResults.map(_.toSet).reduce(_ ++ _)
+    val sorted = merged.toSeq.sortBy(-_.occurrence().toInt)
+    sorted.take(5)
   }
 
 }
