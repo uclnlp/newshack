9e34bd26b3a6372ccbfea88f0ce3877af7db4fd3
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2013-10-17 20:36:21 +0100

    storyfinder gives you first stories in which *all* or some subset of the requested entities occur

--- src/main/scala/StoryFinder.scala
+++ src/main/scala/StoryFinder.scala
@@ -10,7 +10,7 @@ import scalaj.http.{HttpOptions, Http}
 object StoryFinder extends App {
   class JuicerResult extends Frontlet {
     val stories = FrontletListSlot("stories", () => new Story)
-    val refId = StringSlot("refId")
+    val refId = StringListSlot("refId")
   }
 
   class Article extends Frontlet {
@@ -23,14 +23,42 @@ object StoryFinder extends App {
     val date = StringSlot("date")
     val title = StringSlot("title")
     val tagged = FrontletListSlot("tagged", () => new Article)
+  }  
+
+  def queryCombinations(ids: Seq[String], numCombinations: Int = 5) = {
+    val queryCombinations =
+      if (ids.size < numCombinations && ids.size >= 2) {
+        for {
+          i <- (2 to numCombinations).reverse
+          tags <- ids.combinations(i)
+        } yield queryWithMultipleIds(tags)        
+      } else Nil  
+    val combinedQuery = queryWithMultipleIds(ids)
+    val singleQueries = ids.map(id => queryWithMultipleIds(List(id)))
+    val result = (queryCombinations ++ singleQueries).filter(story => !story.stories().isEmpty)
+
+    if (ids.size > numCombinations && !combinedQuery.stories().isEmpty) combinedQuery :: result.toList
+    else result.toList
   }
-
-
-  def storyQueryWithMultipleIds(ids:List[String], limit:Int = 5, numStories:Int = 3, numArticles: Int = 5) = {
-    val combinedStories = storyQuery(ids.mkString("tagop=and&","&",""), limit, numStories, numArticles)
-    val individualStories = ids.map(id => storyQuery(id, limit, numStories, numArticles))
-    if (!combinedStories.stories().isEmpty) combinedStories :: individualStories
-    else individualStories
+  
+  def queryWithMultipleIds(ids:Seq[String], limit:Int = 5, numStories:Int = 3, numArticles: Int = 5, numIdCombinations: Int = 5) = {
+    val parameters = ids.map(id => ("tag",id)).toList ++
+      List("class" -> "http://purl.org/ontology/storyline/Storyline",
+           "limit" -> limit.toString,
+           "tagop" -> "and")
+    val result = Http("http://triplestore.bbcnewslabs.co.uk//api/things").
+      option(_.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2")).
+      option(HttpOptions.connTimeout(10000)).
+      option(HttpOptions.readTimeout(10000)).params(parameters)
+    
+    val stories = new JuicerResult().setJSON("{\"stories\": " + result.asString + "}")
+    // filtering stories
+    val filteredStories = stories.stories().take(numStories).map(story => {
+      story.tagged := story.tagged().take(numArticles)
+    })
+    val resultStories = new JuicerResult().stories(filteredStories)
+    resultStories.refId := ids
+    resultStories
   }
 
   def storyQuery(id:String, limit:Int = 5, numStories:Int = 3, numArticles: Int = 5) = {
@@ -38,7 +66,7 @@ object StoryFinder extends App {
       option(_.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2")).
       option(HttpOptions.connTimeout(10000)).
       option(HttpOptions.readTimeout(10000)).params(
-        "tag" -> id,
+        "id" -> id,
         "class" -> "http://purl.org/ontology/storyline/Storyline",
         "limit" -> limit.toString
       )
@@ -48,7 +76,7 @@ object StoryFinder extends App {
       story.tagged := story.tagged().take(numArticles)
     })
     val resultStories = new JuicerResult().stories(filteredStories)
-    resultStories.refId := id
+    resultStories.refId := List(id)
     resultStories
   }
 
@@ -58,24 +86,25 @@ object StoryFinder extends App {
   }
 
   val dbpediaIds = args
-  val stories = searchStory(dbpediaIds)
+  //val stories = searchStory(dbpediaIds)
+  val stories = queryCombinations(dbpediaIds)
   println(stories.mkString("\n"))
-  stories.foreach(story => {
-    println("Title: " + story.title())
+  stories.foreach(s => {
+    println("Ids: " + s.refId().mkString(", "))
+    s.stories().foreach(story => {
+    println("\tTitle: " + story.title())
     story.tagged().foreach(article => {
-      println("\tArticle: " + article.title())
-      println("\t\tURI:" + article.uri())
+      println("\t\tArticle: " + article.title())
+      println("\t\t\tURI:" + article.uri())
     })
-  })
+  })}
+  )
 }
 
 /*
 Examples:
-http://localhost:8888/find/story?id=http://dbpedia.org/resource/David_Cameron
-http://localhost:8888/find/story?id=http://dbpedia.org/resource/David_Cameron&id=http://dbpedia.org/resource/Barack_Obama
-http://localhost:8888/find/story?id=http://dbpedia.org/resource/David_Cameron&id=http://dbpedia.org/resource/Syria&limit=5
 
-TODO:
-• AND operator for DBpedia ids
-/api/things?tagop=and&tag=http://dbpedia.org/resource/David_Cameron&tag=http://dbpedia.org/resource/Syria&limit=5
+/find/story?id=http://dbpedia.org/resource/David_Cameron&id=http://dbpedia.org/resource/Nigel_Farage&limit=5
+
+will give you the storyline with David and Nigel bevore storylines with only one of them
 */
