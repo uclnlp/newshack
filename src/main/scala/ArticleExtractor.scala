86fd4e507383f0334acc0477fe8e172435b24e3e
Giorgos <geospith@gmail.com>
2013-10-17 16:02:52 +0100

    My awesome scala file ^_^

---
+++ src/main/scala/ArticleExtractor.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Giorgos
+ * Date: 17/10/13
+ * Time: 15:59
+ * To change this template use File | Settings | File Templates.
+ */
+class ArticleExtractor {
+
+}
