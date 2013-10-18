28e7037f9e1c9076fd31914c371f137c3b4a59f6
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2013-10-18 02:59:29 +0100

    only handling BBC websites right now

--- src/main/scala/ArticleParser.scala
+++ src/main/scala/ArticleParser.scala
@@ -11,26 +11,37 @@ import scala.xml.Node
 object ArticleParser {
 
   def extract(URL: String): Array[String] = {
-    val html = io.Source.fromURL(URL).mkString
-    val parser = new HTML5Parser
-    val parsed = parser.loadString(html)
-    var story: Node = null
-    var article= Array("", "")
-    for (n <- (parsed \\ "div")) {
-      if ((n \ "@class").text == "story-body")
-        story = n
-    }
-    for (n <- (story \\ "h1"))
-      if ((n \ "@class").text == "story-header")
-        article(0) = n.text
-    article(1) = (story \\ "p").map(_.text).filter(_ != "Please turn on JavaScript. Media requires JavaScript to play.").mkString(" ")
-    article
+    if (URL.startsWith("http://www.bbc.co.uk/news")) {
+      print("scanning " + URL + "... ")
+      val start = System.currentTimeMillis()
+      //println("opening connection...")
+      val html = io.Source.fromURL(URL).mkString
+      println((System.currentTimeMillis() - start) + "ms")
+      val start2 = System.currentTimeMillis()
+      //println("parsing...")
+      val parser = new HTML5Parser
+      val parsed = parser.loadString(html)
+      var story: Node = null
+      var article= Array("", "")
+      for (n <- (parsed \\ "div")) {
+        if ((n \ "@class").text == "story-body")
+          story = n
+      }
+      try {
+        for (n <- (story \\ "h1"))
+          if ((n \ "@class").text == "story-header")
+            article(0) = n.text
+        article(1) = (story \\ "p").map(_.text).filter(_ != "Please turn on JavaScript. Media requires JavaScript to play.").mkString(" ")
+      } catch { case ne:NullPointerException => /* ignore */ }
+      println("parsing took " + (System.currentTimeMillis() - start2) + "ms")
+      article
+    } else Array("")
   }
 
   def main(args: Array[String]) {
     //val URL="http://www.bbc.co.uk/news/uk-politics-24553611"
     val URL = "http://www.bbc.co.uk/news/uk-england-derbyshire-24548690"
-    val article=extract(URL)
+    val article = extract(URL)
     println("TITLE: " + article(0))
     println("BODY: " + article(1))
   }
