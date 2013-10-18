fca291dd331b89dcac4eab2a51408bdc8422590e
Giorgos <geospith@gmail.com>
2013-10-18 02:52:26 +0100

    handle similarity in case of empty string

--- src/main/scala/ArticleParser.scala
+++ src/main/scala/ArticleParser.scala
@@ -10,19 +10,25 @@ import scala.xml.Node
 
 object ArticleParser {
 
-  def similarity( a:Map[String,Int],b:Map[String,Int] ): Double = {
-    val asize=Math.sqrt(a.mapValues(x=>x*x).foldLeft(0)(_+_._2))
-    val bsize=Math.sqrt(b.mapValues(x=>x*x).foldLeft(0)(_+_._2))
-    val prod = (a.keySet ++ b.keySet).map (i=> (i,a.getOrElse(i,0)*b.getOrElse(i,0))).toMap.foldLeft(0)(_+_._2)
-    prod/(asize*bsize)
+  def similarity(a: Map[String, Int], b: Map[String, Int]): Double = {
+    if (a.isEmpty || b.isEmpty)
+      0
+    else {
+      val asize = Math.sqrt(a.mapValues(x => x * x).foldLeft(0)(_ + _._2))
+      val bsize = Math.sqrt(b.mapValues(x => x * x).foldLeft(0)(_ + _._2))
+      val prod = (a.keySet ++ b.keySet).map(i => (i, a.getOrElse(i, 0) * b.getOrElse(i, 0))).toMap.foldLeft(0)(_ + _._2)
+      prod / (asize * bsize)
+    }
   }
-  def time(f: => Unit)={
+
+  def time(f: => Unit) = {
     val s = System.currentTimeMillis
     f
     System.currentTimeMillis - s
   }
+
   def extract(URL: String): Array[String] = {
-    var article= Array("", "")
+    var article = Array("", "")
     val html = io.Source.fromURL(URL).mkString
     val parser = new HTML5Parser
     val parsed = parser.loadString(html)
@@ -38,35 +44,35 @@ object ArticleParser {
     article
   }
 
-  def getNormVector(article:String,stopwords:Array[String]):Map[String,Int]={
-    val symbols = Seq(".", ",", "(", ")", "\"","[","]", "!", "'")
-    var doc =article.toLowerCase()
+  def getNormVector(article: String, stopwords: Array[String]): Map[String, Int] = {
+    val symbols = Seq(".", ",", "(", ")", "\"", "[", "]", "!", "'")
+    var doc = article.toLowerCase()
     //remove punctuation
     for (symbol <- symbols) {
-      doc=doc.replace(symbol, "")
+      doc = doc.replace(symbol, "")
     }
-    var seq=doc.split(" ")
-    for (word<-stopwords){
-      seq=seq.filter(_!=word)
+    var seq = doc.split(" ")
+    for (word <- stopwords) {
+      seq = seq.filter(_ != word)
     }
     seq.groupBy(identity).mapValues(_.length)
   }
 
   def main(args: Array[String]) {
-    val URL1="http://www.bbc.co.uk/news/uk-24530186"
+    val URL1 = "http://www.bbc.co.uk/news/uk-24530186"
     val URL2 = "http://www.bbc.co.uk/news/uk-politics-24553611"
-    val URL3 ="http://www.bbc.co.uk/news/uk-24575059"
-    val stopwords =io.Source.fromURL("http://www.textfixer.com/resources/common-english-words.txt").mkString.split(",")
+    val URL3 = "http://www.bbc.co.uk/news/uk-24575059"
+    val stopwords = io.Source.fromURL("http://www.textfixer.com/resources/common-english-words.txt").mkString.split(",")
     val article1 = extract(URL1)
     val article2 = extract(URL2)
     val article3 = extract(URL3)
     //println("TITLE: " + article1(0))
     //println("BODY: " + article1(1))
-    val mapA=getNormVector(article1.mkString(" "),stopwords)
-    val mapB=getNormVector(article2.mkString(" "),stopwords)
-    val mapC=getNormVector(article3.mkString(" "),stopwords)
-    println(similarity(mapA,mapB))
-    println(similarity(mapA,mapC))
+    val mapA = getNormVector(article1.mkString(" "), stopwords)
+    val mapB = getNormVector(article2.mkString(" "), stopwords)
+    val mapC = getNormVector(article3.mkString(" "), stopwords)
+    println(similarity(mapA, mapB))
+    println(similarity(mapA, mapC))
   }
 
 }
