f7056f369145d9934c0064e8bbc7b12455ff7cae
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2013-10-19 16:57:43 +0100

    still can' find a simple example of the bug I encountered yesterday

--- src/test/scala/FrontletTest.scala
+++ src/test/scala/FrontletTest.scala
@@ -9,12 +9,14 @@ import org.riedelcastro.frontlets.Frontlet
 object FrontletTest extends App {
   class BasicFrontlet extends Frontlet {
     val number = IntSlot("number")
+    val otherNumber = IntSlot("otherNumber")
     override def toString() = number().toString
   } 
   
   class CompositeFrontlet extends Frontlet {
     val basics = FrontletListSlot("basics", () => new BasicFrontlet)
-    override def toString() = if (basics().isEmpty) "Nil" else basics().mkString(", ")
+    val title = StringSlot("title")
+    override def toString() = "#" + title() + "[" + (if (basics().isEmpty) "Nil" else basics().mkString(", ")) + "]"
   }
 
   class SuperFrontlet extends Frontlet {
@@ -22,13 +24,13 @@ object FrontletTest extends App {
     override def toString() = composites().mkString(" | ")
   }
   
-  val b1 = new BasicFrontlet; b1.number := 5
-  val b2 = new BasicFrontlet; b2.number := 4
-  val b3 = new BasicFrontlet; b3.number := 7
-  val comp1 = new CompositeFrontlet; comp1.basics := List(b1,b2,b3)
-  val b4 = new BasicFrontlet; b4.number := 2
-  val b5 = new BasicFrontlet; b5.number := 5
-  val comp2 = new CompositeFrontlet; comp2.basics := List(b4,b5)
+  val b1 = new BasicFrontlet; b1.number := 5; b1.otherNumber := 5
+  val b2 = new BasicFrontlet; b2.number := 4; b2.otherNumber := 4
+  val b3 = new BasicFrontlet; b3.number := 7; b3.otherNumber := 7
+  val comp1 = new CompositeFrontlet; comp1.basics := List(b1,b2,b3); comp1.title := "first"
+  val b4 = new BasicFrontlet; b4.number := 2; b4.otherNumber := 2
+  val b5 = new BasicFrontlet; b5.number := 5; b5.otherNumber := 5
+  val comp2 = new CompositeFrontlet; comp2.basics := List(b4,b5); comp2.title := "second"
   val frontception = new SuperFrontlet; frontception.composites := List(comp1,comp2)
 
   println(s"comp1: $comp1") //fine
@@ -37,7 +39,9 @@ object FrontletTest extends App {
   comp1.basics := comp1.basics().filter(_.number() > 5)
   println(s"comp1 after filter: $comp1") //fine
   comp1.basics().foreach(c => c.number := 5)
-  println(s"comp1 after foreach: $comp1") //not working
+  println(s"comp1 after foreach: $comp1") //TODO: not working???
+  //comp1.basics().foreach(c => println(c.otherNumber()))
+  //println(s"comp1 after foreach: $comp1")
 
   println(s"comp2: $comp2") //fine
   println(s"frontception: $frontception") //fine
@@ -48,11 +52,25 @@ object FrontletTest extends App {
   })
   println(s"frontception after map: $frontception") //fine
 
+  println(s"frontception sum: ${frontception.composites().map(c => c.basics().map(_.number()).sum).mkString(", ")}") //fine
+
   frontception.composites := frontception.composites().map(composite => {
     composite.basics := composite.basics().filter(_.number() > 6)
   })
   println(s"frontception after filter: $frontception") //fine
 
-  frontception.composites := frontception.composites().filterNot(_.basics().isEmpty)
-  println(s"frontception after non-empty filter: $frontception") //fine
+//  frontception.composites := frontception.composites().filterNot(_.basics().isEmpty)
+//  println(s"frontception after non-empty filter: $frontception") //fine
+
+
+  //trying to reproduce the error during BBC newsHACK
+  //val bucket = frontception.composites()
+  val bucket = List(frontception)
+  bucket.toSeq.foreach(f => {
+    //println(f.composites().size)
+    f.composites().foreach(c => println(c.title()))
+    println(f.composites().size)
+  })
+  println(s"comp1 after frontception: $comp1")
+
 }
